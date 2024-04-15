629b907f559b99a73f4910bd387c9855be5e8eed
鄭仁安 <cat1022043@gmail.com>
2024-04-15 10:04:15 +0800

    Upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
+// PWA
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register(
+	'/Hobe-Fort-Travel/service-worker.js', {
+	  scope: '/Hobe-Fort-Travel/'
+	}
+  )
+}
+
+
+
+
 function AtOnload() {
-	var js_dist = ['/assets/js/mdb.umd.min.js']
+	var js_dist = ['/Hobe-Fort-Travel/assets/js/mdb.umd.min.js']
 	var i=0;
 	for (i=0;i<js_dist.length;i++){
 		let element = document.createElement("script");
@@ -11,32 +23,34 @@ function AtOnload() {
 	
 	// JavaScript Document// Wrap every letter in a span
 	var textWrapper = document.querySelector('.ml1 .letters');
-	textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+	if (textWrapper != undefined) {
+		textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-	anime.timeline({loop: true})
-	  .add({
-		targets: '.ml1 .letter',
-		scale: [0.3,1],
-		opacity: [0,1],
-		translateZ: 0,
-		easing: "easeOutExpo",
-		duration: 600,
-		delay: (el, i) => 70 * (i+1)
-	  }).add({
-		targets: '.ml1 .line',
-		scaleX: [0,1],
-		opacity: [0.5,1],
-		easing: "easeOutExpo",
-		duration: 700,
-		offset: '-=875',
-		delay: (el, i, l) => 80 * (l - i)
-	  }).add({
-		targets: '.ml1',
-		opacity: 0,
-		duration: 1000,
-		easing: "easeOutExpo",
-		delay: 1000
-	  });
+		anime.timeline({loop: true})
+		  .add({
+			targets: '.ml1 .letter',
+			scale: [0.3,1],
+			opacity: [0,1],
+			translateZ: 0,
+			easing: "easeOutExpo",
+			duration: 600,
+			delay: (el, i) => 70 * (i+1)
+		  }).add({
+			targets: '.ml1 .line',
+			scaleX: [0,1],
+			opacity: [0.5,1],
+			easing: "easeOutExpo",
+			duration: 700,
+			offset: '-=875',
+			delay: (el, i, l) => 80 * (l - i)
+		  }).add({
+			targets: '.ml1',
+			opacity: 0,
+			duration: 1000,
+			easing: "easeOutExpo",
+			delay: 1000
+		  });
+	}
 	
 	
 	
